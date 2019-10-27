62a33811e772379704f848014a309421fe233ef7
Rahul Gala <rgala38@gmail.com>
2019-10-27 18:54:53 +0530

    Updates

---
+++ precache-manifest.efd911c7d209c478cb06db5117acfbad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96609f7e34e71c4c518f105a16d3136a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac41767b7061f019a100",
+    "url": "/monsters-rolodex/static/css/main.4e066236.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "ac41767b7061f019a100",
+    "url": "/monsters-rolodex/static/js/main.c4a70b3e.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
